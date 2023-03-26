5f1be59b1195dae8d30a3cb561a45722927b9eaa
create-to-develop <a.dubinnikova.tr@gmail.com>
2023-03-27 00:08:40 +0300

    refactor: delete lib react-bootstrap

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { x } from './Form/Form';
 
 interface CardProps {
   item: {
@@ -18,24 +15,24 @@ interface CardProps {
 export default class CardItem extends React.Component<CardProps> {
   render() {
     return (
-      <>
-        {x?.map((i: CardProps) => (
-          <Card key={i?.item.zipCode} style={{ width: '22rem', margin: '0.5rem' }}>
-            <Card.Body>
-              <Card.Title>Name: {i?.item.name}</Card.Title>
-              <Card.Text>
-                <p>Name: {i?.item.name}</p>
-                <p>Surname: {i?.item.surname}</p>
-                <p>Zip Code: {i?.item.zipCode}</p>
-                <p>Birthday: {i?.item.birthday}</p>
-                <p>Delivery Date: {i?.item.deliveryDate}</p>
-                <p>Country: {i?.item.country}</p>
-                <p>State: {i?.item.state}</p>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        ))}
-      </>
+      <></>
+      //   {x?.map((i: CardProps) => (
+      //     <Card key={i?.item.zipCode} style={{ width: '22rem', margin: '0.5rem' }}>
+      //       <Card.Body>
+      //         <Card.Title>Name: {i?.item.name}</Card.Title>
+      //         <Card.Text>
+      //           <p>Name: {i?.item.name}</p>
+      //           <p>Surname: {i?.item.surname}</p>
+      //           <p>Zip Code: {i?.item.zipCode}</p>
+      //           <p>Birthday: {i?.item.birthday}</p>
+      //           <p>Delivery Date: {i?.item.deliveryDate}</p>
+      //           <p>Country: {i?.item.country}</p>
+      //           <p>State: {i?.item.state}</p>
+      //         </Card.Text>
+      //       </Card.Body>
+      //     </Card>
+      //   ))}
+      // </>
     );
   }
 }
