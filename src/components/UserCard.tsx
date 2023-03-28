e3b839b11df9ae24bee0553a1e351c43a25c297c
create-to-develop <a.dubinnikova.tr@gmail.com>
2023-03-28 19:49:43 +0300

    refactor: change user card: add field names

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -18,11 +18,11 @@ export default class UserCard extends React.Component<CardType, object> {
     return (
       <div className="form-card">
         <div className="form-card__about">
-          <div className="form-card__name">{card.name}</div>
-          <div className="form-card__name">{card.birthday}</div>
-          <div className="form-card__name">{card.country}</div>
-          <div className="form-card__name">{card.gender}</div>
-          <div className="form-card__name">{strNotification}</div>
+          <div className="form-card__name">Name: {card.name}</div>
+          <div className="form-card__name">Birthday: {card.birthday}</div>
+          <div className="form-card__name">Country: {card.country}</div>
+          <div className="form-card__name">Gender: {card.gender}</div>
+          <div className="form-card__name">Gift: {strNotification}</div>
           <img className="form-card__img" src={card.picture} alt="picture" />
         </div>
       </div>
